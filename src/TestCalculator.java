4ccf9b74362d7aef2b7072db24be29cae7c15b80
Worachet Uttha <wuttha@webmail.npru.ac.th>
2024-01-04 17:08:21 +0700

    first commit

---
+++ src/TestCalculator.java
@@ -0,0 +1,14 @@
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator();
+		c1.setValue(4, 3);
+		c1.add();
+		System.out.println(c1.showResult());
+		c1.multiply();
+		System.out.println(c1.showResult());
+
+	}
+
+}
